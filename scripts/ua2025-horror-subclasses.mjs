eca87155ac2fa66e66df425e67efbb9521710a49
Henry Malinowski <henry-malinowski@users.noreply.github.com>
2026-03-31 15:29:32 -0500

    added automatically opening Journal Changelog

--- scripts/ua2025-horror-subclasses.mjs
+++ scripts/ua2025-horror-subclasses.mjs
@@ -1,5 +1,29 @@
+const MODULE_ID = "UA2025-HorrorSubclasses";
+
 Hooks.once("init", () => {
-	console.log("ua2025-horror-subclasses.js hooked");
+	game.settings.register(MODULE_ID, "lastVersion", {
+		name: "Last Version",
+		hint: "The last version checked against to determine whether to show the changelog.",
+		scope: "world",
+		config: false,
+		type: String,
+		default: "1.0.0",
+	});
+
+	console.log(`${MODULE_ID}.mjs hooked`);
+});
+
+Hooks.once("ready", async () => {
+	const currentVersion = game.modules.get(MODULE_ID).version;
+	const lastVersion = game.settings.get(MODULE_ID, "lastVersion");
+	if (foundry.utils.isNewerVersion(currentVersion, lastVersion)) {
+		const journal = await fromUuid(
+			"Compendium.UA2025-HorrorSubclasses.content.JournalEntry.uaChangelog00000",
+		);
+		const page = journal.pages.contents.at(-1);
+		journal.sheet.render(true, { pageId: page.id });
+		game.settings.set(MODULE_ID, "lastVersion", currentVersion);
+	}
 });
 
 /**
